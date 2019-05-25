0b7d416bc0eed53f895307f26c8669376b9711c5
tw5033 <tw5033@gmail.com>
2019-05-25 00:33:47 +0000

    Add dotenv for sensitive info

--- web/src/components/Movie/Form.tsx
+++ web/src/components/Movie/Form.tsx
@@ -1,22 +1,41 @@
 import * as React from 'react';
 const { useState, useEffect } = React;
+import axios from 'axios';
 
 
 const Form: React.FunctionComponent<{}> = props => {
   const [formInput, setFormInput] = useState('')
+  const [data, setData] = useState({ hits: [] });
   const youtubeSearch =  (e) => {
     e.preventDefault();
     const search = e.target.elements.youtubeSearch.value;
     console.log(search)
   }
 
-  const onKeyUp = (e) => {
+  // const onKeyUp = (e) => {
     
-  }
+  // }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+       `https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?
+        part=snippet
+        &order=viewCount
+        &q=skateboarding+dog
+        &type=video
+        &videoDefinition=high`
+      );
+      setData(result.data);
+      console.log(`Youtube returned ${data}`)
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
     <form onSubmit={youtubeSearch}>
-        <input type="text" name="youtubeSearch" value='formInput' onKeyUp={}/>
+        <input type="text" name="youtubeSearch" value='formInput'/>
         <button>Search</button>
     </form>
     </>
