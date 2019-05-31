1d5935423cdcba18c6f08be0c14f4b35cc102115
tw5033 <tw5033@gmail.com>
2019-05-31 18:40:13 +0000

    Format fixes for code, commented out hardcoded data to test different videos

--- web/src/components/Movie/Form.tsx
+++ web/src/components/Movie/Form.tsx
@@ -8,88 +8,88 @@ const Form: React.FunctionComponent<{addToPlaylist(url: string): void, sendMessa
   const [formInput, setFormInput] = useState('');
   const [resultVisibility, setResultVisibility] = useState('container is-overlay is-relative is-hidden');
   const [data, setData] = useState([
-    {
-      "kind": "youtube#searchResult",
-      "etag": "\"XpPGQXPnxQJhLgs6enD_n8JR4Qk/ECfaGbrSVac9-5lbIs2zkqKFcwA\"",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "nM0xDI5R50E"
-        },
-      "snippet": {
-        "publishedAt": "2018-10-10T09:00:06.000Z",
-        "channelId": "UCweOkPb1wVVH0Q0Tlj4a5Pw",
-        "title": "[MV] IU(아이유) _ BBIBBI(삐삐)",
-        "description": "[MV] IU(아이유) _ BBIBBI(삐삐) *English subtitles are now available. :D (Please click on 'CC' button or activate 'Interactive Transcript' function) [Notice] 1theK ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/default.jpg",
-            "width": 120,
-            "height": 90
-            },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-            },
-          "high": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-            }
-        },
-        "channelTitle": "1theK (원더케이)",
-        "liveBroadcastContent": "none"
-        }
-    },{
-      "kind": "youtube#searchResult",
-      "etag": "\"XpPGQXPnxQJhLgs6enD_n8JR4Qk/ECfaGbrSVac9-5lbIs2zkqKFcwA\"",
-      "id": {
-        "kind": "youtube#video",
-        "videoId": "ySMGudnpdXQ"
-        },
-      "snippet": {
-        "publishedAt": "2018-10-10T09:00:06.000Z",
-        "channelId": "UCweOkPb1wVVH0Q0Tlj4a5Pw",
-        "title": "[MV] IU(아이유) _ BBIBBI(삐삐)",
-        "description": "[MV] IU(아이유) _ BBIBBI(삐삐) *English subtitles are now available. :D (Please click on 'CC' button or activate 'Interactive Transcript' function) [Notice] 1theK ...",
-        "thumbnails": {
-          "default": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/default.jpg",
-            "width": 120,
-            "height": 90
-            },
-          "medium": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/mqdefault.jpg",
-            "width": 320,
-            "height": 180
-            },
-          "high": {
-            "url": "https://i.ytimg.com/vi/nM0xDI5R50E/hqdefault.jpg",
-            "width": 480,
-            "height": 360
-            }
-        },
-        "channelTitle": "1theK (원더케이)",
-        "liveBroadcastContent": "none"
-        }
-    }
+    // {
+    //   "kind": "youtube#searchResult",
+    //   "etag": "\"XpPGQXPnxQJhLgs6enD_n8JR4Qk/ECfaGbrSVac9-5lbIs2zkqKFcwA\"",
+    //   "id": {
+    //     "kind": "youtube#video",
+    //     "videoId": "nM0xDI5R50E"
+    //     },
+    //   "snippet": {
+    //     "publishedAt": "2018-10-10T09:00:06.000Z",
+    //     "channelId": "UCweOkPb1wVVH0Q0Tlj4a5Pw",
+    //     "title": "[MV] IU(아이유) _ BBIBBI(삐삐)",
+    //     "description": "[MV] IU(아이유) _ BBIBBI(삐삐) *English subtitles are now available. :D (Please click on 'CC' button or activate 'Interactive Transcript' function) [Notice] 1theK ...",
+    //     "thumbnails": {
+    //       "default": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/default.jpg",
+    //         "width": 120,
+    //         "height": 90
+    //         },
+    //       "medium": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/mqdefault.jpg",
+    //         "width": 320,
+    //         "height": 180
+    //         },
+    //       "high": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/hqdefault.jpg",
+    //         "width": 480,
+    //         "height": 360
+    //         }
+    //     },
+    //     "channelTitle": "1theK (원더케이)",
+    //     "liveBroadcastContent": "none"
+    //     }
+    // },{
+    //   "kind": "youtube#searchResult",
+    //   "etag": "\"XpPGQXPnxQJhLgs6enD_n8JR4Qk/ECfaGbrSVac9-5lbIs2zkqKFcwA\"",
+    //   "id": {
+    //     "kind": "youtube#video",
+    //     "videoId": "ySMGudnpdXQ"
+    //     },
+    //   "snippet": {
+    //     "publishedAt": "2018-10-10T09:00:06.000Z",
+    //     "channelId": "UCweOkPb1wVVH0Q0Tlj4a5Pw",
+    //     "title": "[MV] IU(아이유) _ BBIBBI(삐삐)",
+    //     "description": "[MV] IU(아이유) _ BBIBBI(삐삐) *English subtitles are now available. :D (Please click on 'CC' button or activate 'Interactive Transcript' function) [Notice] 1theK ...",
+    //     "thumbnails": {
+    //       "default": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/default.jpg",
+    //         "width": 120,
+    //         "height": 90
+    //         },
+    //       "medium": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/mqdefault.jpg",
+    //         "width": 320,
+    //         "height": 180
+    //         },
+    //       "high": {
+    //         "url": "https://i.ytimg.com/vi/nM0xDI5R50E/hqdefault.jpg",
+    //         "width": 480,
+    //         "height": 360
+    //         }
+    //     },
+    //     "channelTitle": "1theK (원더케이)",
+    //     "liveBroadcastContent": "none"
+    //     }
+    // }
   ]);
   const onInput = event => {
     setFormInput(event.target.value);
   }
 
   const onKeyUp = event => {
-    setResultVisibility('container is-overlay is-relative')
-    // if(event.key === 'Enter') {
-    //   axios.get(
-    //     process.env.URL + `/api/youtube/${formInput}`, {
-    //     }).then(result => {
-    //       console.log('Received response');
-    //       setData(result.data);
-    //       setFormInput('');
-    //       setResultVisibility('container is-overlay is-relative')
-    //   }).catch(err => console.error('Failed to retrieve search data'));
-    // }
+    // setResultVisibility('container is-overlay is-relative')
+    if(event.key === 'Enter') {
+      axios.get(
+        process.env.URL + `/api/youtube/${formInput}`, {
+        }).then(result => {
+          console.log('Received response');
+          setData(result.data);
+          setFormInput('');
+          setResultVisibility('container is-overlay is-relative')
+      }).catch(err => console.error('Failed to retrieve search data'));
+    }
   }
   // below is code to grab searches on input change
   // useEffect(() => {
